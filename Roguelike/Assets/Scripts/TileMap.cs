e91183457a5916d8ed36e7f7326b0ed992cddf56
TiesNaber <tiesnaber@live.nl>
2017-05-09 14:24:43 +0200

    d

---
+++ Roguelike/Assets/Scripts/TileMap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour {
+
+    TileType[,] tileTypes;
+    int[,] tiles;
+    int mapSizeY = 10;
+    int mapSizeX = 10;
+
+    void Start()
+    {
+        //allocate our map tiles
+        tiles = new int[mapSizeX, mapSizeY];
+        //initialize our map tiles
+        for (int x = 0; x < mapSizeX; x++) {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                tiles[x, y] = 0;
+            }
+        }
+    }
+
+}
