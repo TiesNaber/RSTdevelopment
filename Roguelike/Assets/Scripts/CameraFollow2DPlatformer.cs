af4f722355e2f11c839525b8d2645a8792cf2c2f
Florian <floriankindt@hotmail.com>
2017-05-23 22:37:50 +0200

    with bloodspatter, some sfc, and explosion particles

--- Roguelike/Assets/Scripts/CameraFollow2DPlatformer.cs
+++ Roguelike/Assets/Scripts/CameraFollow2DPlatformer.cs
@@ -4,33 +4,57 @@ using UnityEngine;
 
 public class CameraFollow2DPlatformer : MonoBehaviour {
 
-    public Transform target; //what the camera is following
-    public float smoothing; //dapening effect
+	public Transform target; //what the camera is following
+	public float smoothing; //dapening effect
 
-    Vector3 offset;
-    Camera playerCamera;
-    float lowY;
+	Vector3 offset;
+	Camera playerCamera;
+	float lowY;
+
+	float shake = 0;
+	float shakeAmount = 0.6f;
+	float decreaseFactor = 1;
+
+	public float ShakeMe
+	{
+		set { shake = value; }
+	}
 
 
 	// Use this for initialization
 	void Start () {
 
-        playerCamera = GetComponent<Camera>();
-        offset = transform.position - target.position;
-        lowY = transform.position.y;
+		playerCamera = GetComponent<Camera>();
+		offset = transform.position - target.position;
+		lowY = transform.position.y;
 		
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate ()
-    {
-        //playerCamera.orthographicSize = (Screen.height / 100f) / 1.5f;
+	{
+		if(Input.GetKey(KeyCode.V))
+		{
+			ShakeMe = 1;
+		}
 
-        Vector3 targetCamPos = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
-        if (transform.position.y < lowY)
-        {
-            transform.position = new Vector3(transform.position.x, lowY, transform.position.z);
-        }	
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+		if (transform.position.y < lowY)
+		{
+			transform.position = new Vector3(transform.position.x, lowY, transform.position.z);
+		}
+
+		if (shake > 0)
+		{
+			playerCamera.transform.localPosition = Random.insideUnitSphere * shakeAmount;
+			shake -= Time.deltaTime * decreaseFactor;
+
+		}
+		else
+		{
+			shake = 0;
+		}
+
 	}
 }
