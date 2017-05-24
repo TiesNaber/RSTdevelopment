d83919f6bd78c41acab2df9466165c1c9be4c3e9
TiesNaber <tiesnaber@live.nl>
2017-05-24 09:02:46 +0200

    Merge branch 'master' of https://github.com/TiesNaber/RSTdevelopment
    # Conflicts:
    #	Roguelike/Assets/Scenes/Main.unity

---
+++ Roguelike/Assets/VisualDamage.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualDamage : MonoBehaviour {
+
+	[SerializeField]
+	bool player;                // boolean to see if it is the player or an enemy
+	[SerializeField]
+	GameObject blood;           // is the bloodspater particle
+	[SerializeField]
+	Color blinkColor;           // the color it gets when it blinks
+
+	Color baseColor;            // the color it starts with
+	SpriteRenderer myImage;     // your sprite
+	
+
+	// Use this for initialization
+	void Start () {
+		myImage = GetComponent<SpriteRenderer>();
+		baseColor = myImage.color;
+	}
+
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			MakeItBlink(player);
+		}
+	}
+
+	public void MakeItBlink(bool player)
+	{
+		StartCoroutine(Blink(player));
+	}
+	
+	IEnumerator Blink(bool Player)
+	{
+		//make it blink twice
+		myImage.color = blinkColor;
+
+        Vector3 pos = transform.position;
+        GameObject tempHolder = (GameObject)Instantiate(blood, new Vector3(pos.x, pos.y, pos.z - 0.2f), Quaternion.identity);
+		Destroy(tempHolder, 1);
+
+        if(!player)
+        {
+            Destroy(gameObject);
+        }
+		
+		yield return new WaitForSeconds(0.25f);
+		myImage.color = baseColor;
+	}
+}
---
+++ Roguelike/Assets/WeaponHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponHandler : MonoBehaviour {
+
+    Color baseColor;
+    [SerializeField]
+    Color selectedColor;
+
+    bool leftActive;
+    bool rightActive;
+
+    // Use this for initialization
+    void Start () {
+        baseColor = transform.GetChild(0).GetComponent<Image>().color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            SetWeapon(true);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SetWeapon(false);
+        }
+    }
+
+    void SetWeapon(bool left)
+    {
+        Transform leftSlot = transform.GetChild(0);
+        Transform rightSlot = transform.GetChild(1);
+
+        if (left)
+        {
+            if (leftSlot.childCount > 0)
+            {
+                leftSlot.GetComponent<Image>().color = selectedColor;
+                rightSlot.GetComponent<Image>().color = baseColor;
+                leftSlot.GetChild(0).GetComponent<WeaponScript>().enabled = true;
+
+                if(rightSlot.childCount > 0)
+                    rightSlot.GetChild(0).GetComponent<WeaponScript>().enabled = false;
+            }
+        }
+        else
+        {
+            if (transform.GetChild(1).childCount > 0)
+            {
+                rightSlot.GetComponent<Image>().color = selectedColor;
+                leftSlot.GetComponent<Image>().color = baseColor;
+                rightSlot.GetChild(0).GetComponent<WeaponScript>().enabled = true;
+
+                if (rightSlot.childCount > 0)
+                    leftSlot.GetChild(0).GetComponent<WeaponScript>().enabled = false;
+            }
+        }
+    }
+}
