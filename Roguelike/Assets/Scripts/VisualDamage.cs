1daddb48c7d17c60dd2c10df97507be9a2e96f76
Florian <floriankindt@hotmail.com>
2017-05-24 10:18:55 +0200

    with bullet particles and explosion particles

--- Roguelike/Assets/Scripts/VisualDamage.cs
+++ Roguelike/Assets/Scripts/VisualDamage.cs
@@ -23,10 +23,7 @@ public class VisualDamage : MonoBehaviour {
 
 	public void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.A))
-		{
-			MakeItBlink(player);
-		}
+
 	}
 
 	public void MakeItBlink(bool player)
@@ -39,14 +36,14 @@ public class VisualDamage : MonoBehaviour {
 		//make it blink twice
 		myImage.color = blinkColor;
 
-        Vector3 pos = transform.position;
-        GameObject tempHolder = (GameObject)Instantiate(blood, new Vector3(pos.x, pos.y, pos.z - 0.2f), Quaternion.identity);
+		Vector3 pos = transform.position;
+		GameObject tempHolder = (GameObject)Instantiate(blood, new Vector3(pos.x, pos.y, pos.z - 0.2f), Quaternion.identity);
 		Destroy(tempHolder, 1);
 
-        if(!player)
-        {
-            Destroy(gameObject);
-        }
+		if(!player)
+		{
+			gameObject.SetActive(false);
+		}
 		
 		yield return new WaitForSeconds(0.25f);
 		myImage.color = baseColor;
