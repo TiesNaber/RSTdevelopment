f8ba640cb7869624bcefa1d3529553a4489e774c
Sander Benjamins <sjanne1992@hotmail.com>
2017-05-26 13:25:20 +0200

    Added Melee Animation and sound to the attack

--- Roguelike/Assets/Scripts/MeleeScript.cs
+++ Roguelike/Assets/Scripts/MeleeScript.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Completed;
 using UnityEngine;
 
     public class MeleeScript : MonoBehaviour
     {
 
         public bool canHitEnemy;
+        private Animator animator;                  //Used to store a reference to the Player's animator component.
+        private AudioSource audioSource;
+
 
         // Use this for initialization
         void Start()
         {
-
+            animator = GameObject.Find("Player").GetComponent<Animator>();
+            audioSource = GetComponent<AudioSource>();
         }
 
         // Update is called once per frame
@@ -26,7 +31,8 @@ using UnityEngine;
             {
                 canHitEnemy = true;
                 other.GetComponent<EnemyHealth>().ChangeEnemyHealth = 30;
+                animator.SetTrigger("playerSlash");
+                audioSource.Play();
             }
-
         }
-    }
+}
